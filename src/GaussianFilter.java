3606b19f02804d685435b39ccb7b959608873409
AnaMarijaBakula <ana.marija.bakula@fpmoz.sum.ba>
2025-06-28 00:25:48 +0200

    Initial commit

---
+++ src/GaussianFilter.java
@@ -0,0 +1,113 @@
+import java.awt.image.BufferedImage;
+
+public class GaussianFilter {
+
+    public static float[] createGaussianKernel(int radius) {
+        int size = radius * 2 + 1;
+        float[] kernel = new float[size];
+        float sigma = radius / 3.0f;
+        float sigma22 = 2 * sigma * sigma;
+        float sigmaPi2 = (float)(2 * Math.PI * sigma);
+        float sqrtSigmaPi2 = (float)Math.sqrt(sigmaPi2);
+        float normalization = (float)1.0 / (sqrtSigmaPi2*sigma);
+        float sum = 0;
+
+        for (int i = -radius; i <= radius; i++) {
+            float distance = i * i;
+            kernel[i + radius] = (float)(normalization * (Math.exp(-distance / sigma22)));
+            sum += kernel[i + radius];
+        }
+
+        for (int i = 0; i < size; i++) {
+            kernel[i] /= sum;
+        }
+
+        return kernel;
+    }
+
+
+    public static BufferedImage horizontalBlur(BufferedImage src, float[] kernel) {
+        int width = src.getWidth();
+        int height = src.getHeight();
+        int radius = kernel.length / 2;
+
+        BufferedImage output = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                float r = 0, g = 0, b = 0;
+                float sum = 0;
+                for (int k = -radius; k <= radius; k++) {
+                    int px = x + k;
+                    if (px < 0) px = 0;
+                    if (px >= width) px = width - 1;
+
+                    int rgb = src.getRGB(px, y);
+                    int red = (rgb >> 16) & 0xFF;
+                    int green = (rgb >> 8) & 0xFF;
+                    int blue = rgb & 0xFF;
+
+                    float weight = kernel[k + radius];
+                    r += red * weight;
+                    g += green * weight;
+                    b += blue * weight;
+                    sum += weight;
+                }
+                int finalRed = Math.min(255, Math.max(0, Math.round(r / sum)));
+                int finalGreen = Math.min(255, Math.max(0, Math.round(g / sum)));
+                int finalBlue = Math.min(255, Math.max(0, Math.round(b / sum)));
+
+                int newRgb = (0xFF << 24) | (finalRed << 16) | (finalGreen << 8) | finalBlue;
+                output.setRGB(x, y, newRgb);
+            }
+        }
+        return output;
+    }
+
+    public static BufferedImage verticalBlur(BufferedImage src, float[] kernel) {
+        int width = src.getWidth();
+        int height = src.getHeight();
+        int radius = kernel.length / 2;
+
+        BufferedImage output = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                float r = 0, g = 0, b = 0;
+                float sum = 0;
+
+                for (int k = -radius; k <= radius; k++) {
+                    int py = y + k;
+                    if (py < 0) py = 0;
+                    if (py >= height) py = height - 1;
+
+                    int rgb = src.getRGB(x, py);
+                    int red = (rgb >> 16) & 0xFF;
+                    int green = (rgb >> 8) & 0xFF;
+                    int blue = rgb & 0xFF;
+
+                    float weight = kernel[k + radius];
+                    r += red * weight;
+                    g += green * weight;
+                    b += blue * weight;
+                    sum += weight;
+                }
+
+                int finalRed = Math.min(255, Math.max(0, Math.round(r / sum)));
+                int finalGreen = Math.min(255, Math.max(0, Math.round(g / sum)));
+                int finalBlue = Math.min(255, Math.max(0, Math.round(b / sum)));
+
+                int newRgb = (0xFF << 24) | (finalRed << 16) | (finalGreen << 8) | finalBlue;
+                output.setRGB(x, y, newRgb);
+            }
+        }
+        return output;
+    }
+
+    public static BufferedImage applyGaussianBlur(BufferedImage image, int radius) {
+        float[] kernel = createGaussianKernel(radius);
+        BufferedImage horizontal = horizontalBlur(image, kernel);
+        BufferedImage vertical = verticalBlur(horizontal, kernel);
+        return vertical;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,42 @@
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+
+            BufferedImage originalImage = ImageIO.read(new File("images/originalImage.png"));
+            long startTimeSequential = System.nanoTime();
+
+            int radius = 50;
+            BufferedImage blurred = GaussianFilter.applyGaussianBlur(originalImage, radius);
+
+            long endTimeSequential = System.nanoTime();
+            long totalProcessingTime = (endTimeSequential - startTimeSequential) / 1_000_000;
+
+            ImageIO.write(blurred, "png", new File("images/blurredImageSequential.png"));
+
+            System.out.println("Vrijeme obrade slike sekvencijalno: " + totalProcessingTime + " ms");
+
+            System.out.println("---------------------------------");
+
+            long startTimeParallel = System.nanoTime();
+
+            BufferedImage blurredParallel = GaussianFilterParallel.applyGaussianBlurParallel(originalImage, radius);
+
+            long endTimeParallel = System.nanoTime();
+            long totalProcessingTimeParallel = (endTimeParallel - startTimeParallel) / 1_000_000;
+
+            ImageIO.write(blurredParallel, "png", new File("images/blurredImageParallel.png"));
+
+            System.out.println("Vrijeme obrade slike paralelno: " + totalProcessingTimeParallel + " ms");
+
+
+
+        } catch (IOException e) {
+            System.err.println("Greška pri učitavanju ili spremanju slike: " + e.getMessage());
+        }
+    }
+}
